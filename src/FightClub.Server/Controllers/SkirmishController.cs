005af7a38ac1f92436802832cb5187e1fb764ba2
Denis Babarykin <babarykin.denis@gmail.com>
2022-12-29 01:28:10 +0300

    Html layout for skirmish implemented.

--- src/FightClub.Server/Controllers/SkirmishController.cs
+++ src/FightClub.Server/Controllers/SkirmishController.cs
@@ -6,10 +6,18 @@ namespace FightClub.Server.Controllers;
 [Route("skirmish")]
 public class SkirmishController : Controller
 {
-    [HttpGet("{mainPlayerName}")]
-    public ActionResult Index([FromRoute] string mainPlayerName)
+    [HttpGet("{playerName}")]
+    public ActionResult Index([FromRoute] string playerName)
     {
-        ViewBag.PlayerName = mainPlayerName;
+        ViewBag.PlayerName = playerName;
+        ViewBag.EnemyName = "Серчик";
+        ViewBag.PlayerMaxHp = 1000;
+        ViewBag.PlayerCurrentHp = 350;
+        ViewBag.EnemyMaxHp = 1000;
+        ViewBag.EnemyCurrentHp = 350;
+        ViewBag.CanFight = new Random().Next(0, 3) == 1;
+        ViewBag.GameStarted = new Random().Next(0, 2) == 1;
+        ViewBag.IsAlive = new Random().Next(0, 3) == 1;
         return View();
     }
 
